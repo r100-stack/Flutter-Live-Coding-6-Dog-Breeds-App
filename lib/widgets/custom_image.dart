3a18b7d3ea4deca3549236656eb7a4fe13d14c2f
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-07-24 22:33:17 -0500

    Added rounded corners on CustomImage, but Flexible makes the app rendering slow. You can see the corners being added.

--- lib/widgets/custom_image.dart
+++ lib/widgets/custom_image.dart
@@ -11,19 +11,27 @@ class CustomImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(kDefaultBorderMargin)
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(kDefaultBorderMargin),
-          child: CachedNetworkImage(
-            imageUrl: imageUrl,
-            placeholder: (context, url) => CustomProgressIndicator(),
-            errorWidget: (context, url, error) =>
-                Icon(Icons.broken_image),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Flexible(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(kDefaultBorderMargin)
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(kDefaultBorderMargin),
+                child: CachedNetworkImage(
+                  imageUrl: imageUrl,
+                  placeholder: (context, url) => CustomProgressIndicator(),
+                  errorWidget: (context, url, error) =>
+                      Icon(Icons.broken_image),
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
