3a1c8007c54995827d35b29d187aea5cc003715d
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2021-06-13 16:38:07 -0500

    Rm platform check to stop "unsupported operation" Error: Unsupported operation: Platform._operatingSystem

--- lib/widgets/custom_image.dart
+++ lib/widgets/custom_image.dart
@@ -1,9 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:dog_breeds_app/constants.dart';
 import 'package:dog_breeds_app/widgets/custom_progress_indicator.dart';
 import 'package:flutter/material.dart';
 
-import 'dart:io' show Platform;
+// import 'dart:io' show Platform;
 
 class CustomImage extends StatelessWidget {
   final String imageUrl;
@@ -12,14 +11,14 @@ class CustomImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool isMobile = false;
-    try {
-      if (Platform.isAndroid || Platform.isIOS) {
-        isMobile = true;
-      }
-    } catch (e) {
-      print(e);
-    }
+    bool isMobile = true;
+    // try {
+    //   if (!Platform.isAndroid) {
+    //     isMobile = false;
+    //   }
+    // } catch (e) {
+    //   print(e);
+    // }
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -27,6 +26,7 @@ class CustomImage extends StatelessWidget {
         tag: imageUrl,
         child: isMobile
             ? CachedNetworkImage(
+                // imageUrl: 'https://flutter-cors-proxy.herokuapp.com/' + imageUrl,
                 imageUrl: imageUrl,
                 placeholder: (context, url) => CustomProgressIndicator(),
                 errorWidget: (context, url, error) => Icon(Icons.broken_image))
